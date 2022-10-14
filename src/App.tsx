2202c8ef212a3e94f0cc675e0bf7e51d443850d1
gideonekekeke <gideonekeke64@gmail.com>
2022-10-14 16:34:30 +0100

    props-types

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<Body />
+		</div>
+	);
+};
+
+export default App;
